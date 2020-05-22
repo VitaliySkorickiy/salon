23d163b76d74ba52e8702ac2b239b5cd97cde900
zarias2020 <zarias@yandex.ru>
2020-05-22 16:35:26 +0300

    start

---
+++ app/js/main.js
@@ -0,0 +1,48 @@
+$(function () {
+
+
+  // $('.slider__inner').slick({
+  //   dots: true,
+  //   infinite: true,
+  //   speed: 2000,
+  //   fade: true,
+  //   autoplay: true,
+  //   autoplaySpeed: 3000,
+  //   prevArrow: false,
+  //   nextArrow: false,
+  //   draggable: true,
+  //   easing: true,
+  //   cssEase: 'linear',
+  // });
+
+
+
+  // var nav = $('.header__menu-down');
+
+  // $(window).scroll(function () {
+  // 	if ($(this).scrollTop() > 125) {
+  // 		nav.addClass("f-nav");
+  // 	} else {
+  // 		nav.removeClass("f-nav");
+  // 	}
+  // });
+
+
+  // $('.burger').on('click', function () {
+  //   $('.header__menu-down ul').slideToggle();
+
+  // });
+
+
+
+
+  // new WOW().init();
+
+
+
+
+
+
+
+
+});
