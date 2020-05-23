007df12e0c0c92a2ce207a08c003107801550035
zarias2020 <zarias@yandex.ru>
2020-05-23 18:44:55 +0300

    added slider, about, services, colladg

--- app/js/main.js
+++ app/js/main.js
@@ -1,42 +1,42 @@
 $(function () {
 
 
-  // $('.slider__inner').slick({
-  //   dots: true,
-  //   infinite: true,
-  //   speed: 2000,
-  //   fade: true,
-  //   autoplay: true,
-  //   autoplaySpeed: 3000,
-  //   prevArrow: false,
-  //   nextArrow: false,
-  //   draggable: true,
-  //   easing: true,
-  //   cssEase: 'linear',
-  // });
+  $('.slider__inner').slick({
+
+    infinite: true,
+    speed: 2000,
+    fade: true,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    prevArrow: false,
+    nextArrow: false,
+    draggable: true,
+    easing: true,
+    cssEase: 'linear',
+  });
 
 
 
-  // var nav = $('.header__menu-down');
+  var nav = $('.header__menu-down');
 
-  // $(window).scroll(function () {
-  // 	if ($(this).scrollTop() > 125) {
-  // 		nav.addClass("f-nav");
-  // 	} else {
-  // 		nav.removeClass("f-nav");
-  // 	}
-  // });
-
-
-  // $('.burger').on('click', function () {
-  //   $('.header__menu-down ul').slideToggle();
-
-  // });
+  $(window).scroll(function () {
+  	if ($(this).scrollTop() > 125) {
+  		nav.addClass("f-nav");
+  	} else {
+  		nav.removeClass("f-nav");
+  	}
+  });
 
 
+  $('.burger').on('click', function () {
+    $('.header__menu-down ul').slideToggle();
+
+  });
 
 
-  // new WOW().init();
+
+
+  new WOW().init();
 
 
 
