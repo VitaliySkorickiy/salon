6cd6cadd3625751398a720e51b8e1e7d244721f8
zarias2020 <zarias@yandex.ru>
2020-05-24 15:30:51 +0300

    added price, action

--- app/js/main.js
+++ app/js/main.js
@@ -17,6 +17,23 @@ $(function () {
 
 
 
+  $('.action__slider').slick({
+
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    easing: true,
+    cssEase: 'linear',
+    autoplay: true,
+    autoplaySpeed: 3000,
+    speed: 2000,
+    prevArrow: false,
+    nextArrow: false,
+  });
+
+
+
+
   var nav = $('.header__menu-down');
 
   $(window).scroll(function () {
